77cfd33385bf129eeb0a2021227c89f3fcbb1be6
tvmichael <tvmichael@meta.ua>
2018-02-24 17:12:48 +0200

    temp

--- casualua.web.hosting-test.net/bitrix/templates/eshop_bootstrap_blue/components/bitrix/catalog.smart.filter/.default/template.php
+++ casualua.web.hosting-test.net/bitrix/templates/eshop_bootstrap_blue/components/bitrix/catalog.smart.filter/.default/template.php
@@ -166,10 +166,10 @@ $this->addExternalCss("/bitrix/css/main/font-awesome.css");
 						continue;
 					?>
 					
-					<div class="<?if ($arParams["FILTER_VIEW_MODE"] == "HORIZONTAL"):?>col-xs-6 text-right<?else:?>col-lg-12<?endif?> bx-filter-parameters-box <?if ($arItem["DISPLAY_EXPANDED"]== "Y"):?>bx-active<?endif?>">
+					<div class="<?if ($arParams["FILTER_VIEW_MODE"] == "HORIZONTAL"):?>col-xs-4 col-sm-6 text-right<?else:?>col-lg-12<?endif?> bx-filter-parameters-box <?if ($arItem["DISPLAY_EXPANDED"]== "Y"):?>bx-active<?endif?>">
 						<!-- NAME MODE -->
 						<span class="bx-filter-container-modef"></span>
-						<div class="bx-filter-parameters-box-title">
+						<div class="bx-filter-parameters-box-title hidden-xs">
 							<span class="bx-filter-parameters-box-hint">
 								<?
 								echo GetMessage('CT_BCSF_FILTER_SIZE');
@@ -409,7 +409,9 @@ $this->addExternalCss("/bitrix/css/main/font-awesome.css");
 													}
 													?>
 												</div>
-												<div class="bx-filter-select-arrow"><img src="<?=$templateFolder;?>/images/up-down-16-v.png"></div>
+												<div class="bx-filter-select-arrow">
+													<img src="<?=$templateFolder;?>/images/up-down-16-v.png">
+												</div>
 												<input
 													style="display: none"
 													type="radio"
@@ -644,12 +646,13 @@ $this->addExternalCss("/bitrix/css/main/font-awesome.css");
 							</div>
 							<div style="clear: both"></div>
 						</div>
+						<div class="hidden-xs cs-additional-width"></div>
 					</div>
 
-					<div class="col-xs-6 text-left">
+					<div class="col-xs-8 col-sm-6 text-left">
 						<div class="bx-filter-select-container">
 							<? $key = '1001'; ?>
-							<span class="bx-filter-parameters-box-hint">
+							<span class="cs-filter-parameters-price-title hidden-xs">
 								<? echo GetMessage('CT_BCSF_FILTER_PRICE');	?>	
 							</span>
 							<div class="bx-filter-block">
@@ -681,6 +684,11 @@ $this->addExternalCss("/bitrix/css/main/font-awesome.css");
 									<div class="bx-filter-select-popup" data-role="dropdownContent" style="display: none;">	
 										<ul>
 											<li>
+												<label class="bx-filter-param-label" onclick="smartFilter.selectDropDownItemPrice(this, 'DT')">
+													<? echo GetMessage('CT_BCSF_FILTER_PRICE_DISCOUNT');?>
+												</label>
+											</li>
+											<li>
 												<label class="bx-filter-param-label" onclick="smartFilter.selectDropDownItemPrice(this, 'LTH')">
 													<?echo GetMessage('CT_BCSF_FILTER_PRICE_LOW_TO_HIGH');?>
 												</label>
@@ -689,12 +697,7 @@ $this->addExternalCss("/bitrix/css/main/font-awesome.css");
 												<label class="bx-filter-param-label" onclick="smartFilter.selectDropDownItemPrice(this, 'HTL')">
 													<? echo GetMessage('CT_BCSF_FILTER_PRICE_HIGH_TO_LOW');?>
 												</label>
-											</li>
-											<li>
-												<label class="bx-filter-param-label" onclick="smartFilter.selectDropDownItemPrice(this, 'DT')">
-													<? echo GetMessage('CT_BCSF_FILTER_PRICE_DISCOUNT');?>
-												</label>
-											</li>
+											</li>											
 										</ul>					
 									</div>
 									<div class="bx-filter-select-arrow">
