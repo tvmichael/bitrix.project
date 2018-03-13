7e18ba7c3119caddc43c67f3db3d4dd3793e15d6
tvmichael <tvmichael@meta.ua>
2018-03-13 22:46:35 +0200

    temp

--- alexpools.ru/test_1/index.php
+++ alexpools.ru/test_1/index.php
@@ -1,24 +1,28 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Title");
+$APPLICATION->SetTitle("Результат");
 ?>
 
 <h2>РЕЗУЛЬТАТ</h2>
-
-<?$APPLICATION->IncludeComponent(
-	"bitrix:form.result.view",
-	".default",
+<?
+$APPLICATION->IncludeComponent(
+	"bitrix:form.result.view", 
+	"template_arhicode",
 	Array(
-		"CHAIN_ITEM_LINK" => "",
-		"CHAIN_ITEM_TEXT" => "",
+	"CHAIN_ITEM_LINK" => "",	// Ссылка на дополнительном пункте в навигационной цепочке
+		"CHAIN_ITEM_TEXT" => "",	// Название дополнительного пункта в навигационной цепочке
 		"COMPONENT_TEMPLATE" => ".default",
-		"EDIT_URL" => "result_edit.php",
-		"RESULT_ID" => "12",
-		"SEF_MODE" => "N",
-		"SHOW_ADDITIONAL" => "N",
-		"SHOW_ANSWER_VALUE" => "N",
-		"SHOW_STATUS" => "Y"
-	)
-);?>
+		"EDIT_URL" => "result_edit.php",	// Страница редактирования результата
+		"RESULT_ID" => "12",	// ID результата
+		"SEF_MODE" => "N",	// Включить поддержку ЧПУ
+		"SHOW_ADDITIONAL" => "N",	// Показать дополнительные поля веб-формы
+		"SHOW_ANSWER_VALUE" => "N",	// Показать значение параметра ANSWER_VALUE
+		"SHOW_STATUS" => "Y",	// Показать текущий статус результата
+	),
+	false
+);
+?>
+
+
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
