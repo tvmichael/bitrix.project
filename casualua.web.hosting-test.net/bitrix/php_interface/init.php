9293b1b0143b088610fb2ef00dcd7df1d96fcd2e
Michael TV <imesg.ztdif@gmail.com>
2018-03-29 17:01:58 +0300

    seo

--- casualua.web.hosting-test.net/bitrix/php_interface/init.php
+++ casualua.web.hosting-test.net/bitrix/php_interface/init.php
@@ -166,9 +166,9 @@ function Event_ChangeMyContent(&$content)
 {
 	if(isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/bitrix/') === false && strpos($_SERVER['REQUEST_URI'], 'order/make') === false && $_SERVER['REQUEST_METHOD'] === 'GET')
 		if(empty($_SERVER['HTTP_X_REQUESTED_WITH']) || (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest')) 
-			if(file_exists($_SERVER['DOCUMENT_ROOT'].'seoshield-client-f/main.php'))
+			if(file_exists($_SERVER['DOCUMENT_ROOT'].'seoshield-client/main.php'))
 			{		
-			    include_once($_SERVER['DOCUMENT_ROOT'].'seoshield-client-f/main.php');
+			    include_once($_SERVER['DOCUMENT_ROOT'].'seoshield-client/main.php');
 
 			    if(function_exists('seo_shield_start_cms'))
 			        seo_shield_start_cms();
