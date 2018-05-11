1e5d13362911da92de85c864fc45820868d9c281
tvmichael <tvmichael@meta.ua>
2018-05-11 09:23:01 +0300

    nova poshta

--- casual.web.hosting-test.net/bitrix/templates/eshop_bootstrap_blue/components/bitrix/sale.order.ajax/new_edost/template.php
+++ casual.web.hosting-test.net/bitrix/templates/eshop_bootstrap_blue/components/bitrix/sale.order.ajax/new_edost/template.php
@@ -3,10 +3,10 @@
 $APPLICATION->SetAdditionalCSS($templateFolder."/style_cart.css");
 $APPLICATION->SetAdditionalCSS($templateFolder."/style.css");
 
-$APPLICATION->SetAdditionalCSS("/bitrix/js/main/jquery/jquery-2.1.3.min.js.pagespeed.jm.i6FmF6RqXu.js";
-
+$APPLICATION->SetAdditionalCSS("/bitrix/js/main/jquery/jquery-2.1.3.min.js");
 $APPLICATION->SetAdditionalCSS($templateFolder."/jquery-ui.css");
 $APPLICATION->AddHeadScript($templateFolder."/jquery-ui.js");
+
 
 CJSCore::Init(array('fx', 'popup', 'window', 'ajax'));
 ?>
@@ -176,6 +176,9 @@ if (!function_exists("cmpBySort"))
 				BX.ajax.submitComponentForm(orderForm, 'order_form_content', true);
 				BX.submit(orderForm);
 
+				console.log('NovaPoshta-reload:::.');
+				console.log(JSNovaPoshta);
+				JSNovaPoshta();
 				return true;
 			}
 
@@ -282,3 +285,173 @@ if (!function_exists("cmpBySort"))
 	?>
 	</div>	
 </div>
+
+<?
+$jsParams = array(
+	'CITY_ID' => $arResult["ORDER_PROP"]["USER_PROPS_Y"][6]['FIELD_NAME'].'_val',
+	'STORAGE_ID' => $arResult["ORDER_PROP"]["USER_PROPS_Y"][55]['FIELD_NAME'],
+	'LANG' => 'ua'
+);
+?>
+
+<script type="text/javascript">
+	;(function (window)
+	{
+		window.JSNovaPoshta = function (arParams)
+		{
+			//console.log('JSNovaPoshta');
+			//console.log(arParams);
+			if (arParams)
+			{
+				this.cityID = arParams.CITY_ID;
+				this.storageID = arParams.STORAGE_ID;
+				
+				this.availableCity = [];
+				this.availableCityResponse = [];
+
+				this.availablePostOffice = [];
+
+				this.lang = arParams.LANG; 
+			}
+
+			console.log(this);
+			this.init();
+		}
+
+		window.JSNovaPoshta.prototype = {
+			init: function()
+			{
+				// city
+				this.city = document.getElementById(this.cityID);
+				this.city.value = '';
+
+				$(this.city).autocomplete({ source: this.availableCity});
+
+				$(this.city).keyup(this.cityKeyUp.bind(this));
+
+				var self = this;
+				$(this.city).blur(function(){		
+					self.cityGetWarehouseTypes();
+				});
+				$('#ui-id-1').click(function(){		
+					self.cityGetWarehouseTypes();
+				});
+
+				// postOffice 
+				this.postOffice = document.getElementById(this.storageID);
+				this.postOffice.value = '';
+
+				//$(this.postOffice).autocomplete({ source: this.availablePostOffice });
+				this.inputDataList = document.getElementById('input-text-datalist');
+
+				$(this.postOffice).click(function(e){ console.log(e)});
+
+				console.log(this);
+			},
+
+			cityKeyUp: function(e)
+			{
+				var cityName = e.target.value;		
+				var self = this;	
+
+				//console.log(this);
+				console.log(e);
+				console.log(e.target.value);
+					
+					if (cityName != '')
+					{
+					
+						var settings = {
+							"async": true,
+							"crossDomain": true,
+							"url": "https://api.novaposhta.ua/v2.0/json/",
+							"method": "POST",
+							"headers": {"content-type": "application/json",},
+							"processData": false,
+							"data": "{\r\n\"apiKey\": \"b2444b86ad7faff76b9a69dc6eb37c7d\",\r\n \"modelName\": \"Address\",\r\n \"calledMethod\": \"searchSettlements\",\r\n \"methodProperties\": {\r\n \"CityName\": \""+cityName+"\",\r\n \"Limit\": 10,\r\n \"Language\":\""+this.lang+"\" \r\n }\r\n}"
+						}
+
+						$.ajax(settings).done(function (response) 
+						{
+							console.log(response);		
+
+							var i;						
+							self.availableCityResponse = response;
+
+							if (response.errorCodes.length == 0)
+								for (i = 0; i < response.data[0].Addresses.length; i++)
+								{
+									self.availableCity[i] = response.data[0].Addresses[i].Present;
+								}						
+						});					
+						
+						if (e.keyCode == 13) { this.cityGetWarehouseTypes(); }
+					}
+			},
+
+			cityGetWarehouseTypes: function()
+			{
+				console.log($(this.city).val());
+				console.log(this.availableCity);
+				var i;
+							
+				if (this.availableCityResponse.errorCodes.length == 0)
+				for(i = 0; i < this.availableCityResponse.data[0].Addresses.length; i++ )
+				{
+					if (this.city.value == this.availableCityResponse.data[0].Addresses[i].Present )					
+					{					
+						console.log(this.availableCityResponse.data[0].Addresses[i]);
+						this.postOfficeGetWarehouses(this.availableCityResponse.data[0].Addresses[i].Ref);
+					}
+					console.log(i);				
+				}
+				
+			},
+
+			postOfficeGetWarehouses: function(CityRef)
+			{			
+				var settings = {
+					"async": true,
+					"crossDomain": true,
+					"url": "https://api.novaposhta.ua/v2.0/json/",
+					"method": "POST",
+					"headers": {"content-type": "application/json",},
+					"processData": false,
+					"data": "{\r\n\"apiKey\": \"b2444b86ad7faff76b9a69dc6eb37c7d\",\r\n \"modelName\": \"Address\",\r\n \"calledMethod\": \"getWarehouses\",\r\n \"methodProperties\": {\r\n \"SettlementRef\": \""+CityRef+"\" \r\n }\r\n}"
+				}
+
+				$.ajax(settings).done(this.setPostOffice.bind(this));
+			},
+
+			setPostOffice: function(response)
+			{
+				var i, optionList = '';
+
+				console.log(response);
+				if (response.errorCodes.length == 0)
+				{	
+					for (i = 0; i < response.data.length; i++) {
+						this.availablePostOffice[i] = response.data[i].Description;
+						optionList = optionList + "<option value='" + response.data[i].Description + "'>";
+						this.inputDataList.innerHTML = optionList;
+					}
+
+					if (this.postOffice.value == ''){
+						this.postOffice.value = response.data[0].Description;
+					}
+				}
+			}
+		}
+
+	})(window);
+	
+	 var csNovaPoshta = new JSNovaPoshta(<?=CUtil::PhpToJSObject($jsParams, false, true)?>);
+</script>
+
+<?
+if ( $USER->IsAdmin() && $USER->GetID() == 6 ) { 
+	echo '<div class="col-md-12"><pre>'; 
+	print_r($arResult["ORDER_PROP"]["USER_PROPS_Y"]); 
+	echo '</pre></div>'; 
+};
+?>
