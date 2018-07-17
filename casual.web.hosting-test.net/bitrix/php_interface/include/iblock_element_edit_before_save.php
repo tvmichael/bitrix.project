6d84a339dbd4cb16d4bf133dc055106ac2357b95
tvmichael <tvmichael@meta.ua>
2018-07-17 21:35:14 +0300

    casual

---
+++ casual.web.hosting-test.net/bitrix/php_interface/include/iblock_element_edit_before_save.php
@@ -0,0 +1,18 @@
+<?
+
+if($_SESSION['TEMP_OFFERS_LIST_ID_OPEN'])
+{
+	$minmax = array();
+	foreach ($_SESSION['TEMP_OFFERS_LIST_ID'] as $value) {
+		$ar_res = CPrice::GetBasePrice($value['ID'], false, false);
+		array_push($minmax, $ar_res['PRICE']);
+	}
+	$PROP['78']['n0']['VALUE'] = min($minmax);
+	$PROP['80']['n0']['VALUE'] = max($minmax);
+	$PROP['81']['n0']['VALUE'] = min($minmax);
+ 	$_SESSION['TEMP_OFFERS_LIST_ID'] = array();
+ 	$_SESSION['TEMP_OFFERS_LIST_ID_OPEN'] = false;
+}
+
+	
+?>
